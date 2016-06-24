9719781070db562e4223ac011e3d7d47f975d4af
Eduardo San Martin Morote <posva13@gmail.com>
2016-06-24 20:44:01 +0200

    Add minimal ui

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,35 @@
 <template>
   <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+    <input v-model="lang" @keyup.enter="speak">
+    <br>
+    <textarea v-model="text" @keyup.enter="speak">
+    </textarea>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import fetchival from 'fetchival'
 
 export default {
-  components: {
-    Hello
+  data () {
+    return {
+      lang: 'fr',
+      text: ''
+    }
+  },
+  computed: {
+    textEncoded () {
+      return encodeURIComponent(this.text)
+    }
+  },
+  methods: {
+    speak () {
+      fetchival(`/api/speak/${this.lang}/${this.textEncoded}`).get()
+      .catch(err => {
+        console.error(err)
+      })
+      this.text = ''
+    }
   }
 }
 </script>
