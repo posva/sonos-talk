4b2bd223790556fe3d23aad5c5b4990c02757d4f
Eduardo San Martin Morote <posva13@gmail.com>
2016-06-27 13:34:16 +0200

    Clearer logs

--- server/api.js
+++ server/api.js
@@ -1,22 +1,13 @@
 const Gtts = require('./gtts/gTTS')
-const fs = require('fs')
 const myip = require('quick-local-ip')
 const sonos = require('./sonos')
 const utils = require('./utils')
-
-const sendFileOptions = {
-  dotfiles: 'deny',
-  headers: {
-    'x-timestamp': Date.now(),
-    'x-sent': true
-  }
-}
 
 module.exports = {
   generateSpeech (req, res) {
     const text = utils.decodeText(req.params.file)
 
-    console.log(`Requested ${text}`)
+    console.log(`Requested "${text}"`)
     const gtts = new Gtts(text, req.params.lang)
     res.set('Content-Type', 'audio/mpeg')
     gtts.stream().pipe(res)
@@ -26,7 +17,7 @@ module.exports = {
     const encodedText = utils.encodeText(decodeURI(req.params.text))
     const uri = `/api/generate/${req.params.lang}/${encodedText}.mp3`
     const url = `http://${myip.getLocalIP4()}:${process.env.PORT}${uri}`
-    console.log(`Sending ${url} to Sonos`)
+    console.log(`Sending "${url}" to Sonos`)
     if (sonos.device) {
       sonos.device.play(url, function (err, playing) {
         if (err) return res.status(500).json({ error: 'Sonos error', err })
